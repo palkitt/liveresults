33d3e205d7a3677a484f50b0dcfc3d399a170b84
palkitt <44671818+palkitt@users.noreply.github.com>
2020-05-10 11:48:44 +0200

    Remove last update field in Emma page

--- web/emma/followfull.php
+++ web/emma/followfull.php
@@ -353,7 +353,6 @@ function closeTop() {
 
   <?php if (!$isSingleClass && !$isSingleClub) {?>  
 	<p align="left"><font color="#AAA" size="0.7em">
-Last update: <span id="lastupdate"></span><br>
   * <?=$_HELPREDRESULTS?><br>
   &copy;2012- Liveresults. Source code: https://github.com/palkitt/liveresults</font></p>
   
