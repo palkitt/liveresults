f864d8dc388ef60514e577254b645b7db821f25e
palkitt <44671818+palkitt@users.noreply.github.com>
2022-03-23 21:02:07 +0100

    Make clas sorting case insensitive and Åpen last

--- web/api/api.php
+++ web/api/api.php
@@ -401,7 +401,8 @@ function classesSorted($currentComp){
 		$classSort->name = $class['Class'];
 		$sortKey = strtolower($class['Class']);
 
-		if (strpos($sortKey,'åpen') !== false || strpos($sortKey,'open') !== false || strpos($sortKey,'gjest') !== false || strpos($sortKey,'dir') !== false || strpos($sortKey,'utv') !== false)
+		if (stripos($sortKey,'Åpen') !== false || stripos($sortKey,'åpen') !== false || stripos($sortKey,'open') !== false || 
+	    stripos($sortKey,'gjest') !== false || stripos($sortKey,'dir') !== false || stripos($sortKey,'utv') !== false)
 			$sortKey = 'z'.$sortKey;
 		$sortKey = preg_replace('/(^|[^\d])(\d)($|[^\d])/','$1 00 $2 $3',$sortKey);      // Add 00 ahead of single digits
 		$sortKey = preg_replace('/(^|[^\d])(\d)(\d)($|[^\d])/','$1 0 $2 $3 $4',$sortKey); // Add 0 ahead of double digits
