457639ba2b7d71e51d3f82060a94c0b92cc4d5ff
palkitt <44671818+palkitt@users.noreply.github.com>
2023-01-22 19:32:49 +0100

    Add Brikkesys client

---
+++ LiveResults.Client/NewBrikkesysComp.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace LiveResults.Client
+{
+    partial class NewBrikkesysComp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wizard1 = new Gui.Wizard.Wizard();
+            this.wizardPage5 = new Gui.Wizard.WizardPage();
+            this.txtCompName = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtCompID = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.wizardPage3 = new Gui.Wizard.WizardPage();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbBrikkesysComp = new System.Windows.Forms.ComboBox();
+            this.wizardPage1 = new Gui.Wizard.WizardPage();
+            this.txtPw = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.wizard1.SuspendLayout();
+            this.wizardPage5.SuspendLayout();
+            this.wizardPage3.SuspendLayout();
+            this.wizardPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // wizard1
+            // 
+            this.wizard1.Controls.Add(this.wizardPage5);
+            this.wizard1.Controls.Add(this.wizardPage3);
+            this.wizard1.Controls.Add(this.wizardPage1);
+            this.wizard1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wizard1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wizard1.Location = new System.Drawing.Point(0, 0);
+            this.wizard1.Name = "wizard1";
+            this.wizard1.Pages.AddRange(new Gui.Wizard.WizardPage[] {
+            this.wizardPage1,
+            this.wizardPage3,
+            this.wizardPage5});
+            this.wizard1.Size = new System.Drawing.Size(506, 181);
+            this.wizard1.TabIndex = 0;
+            // 
+            // wizardPage5
+            // 
+            this.wizardPage5.Controls.Add(this.txtCompName);
+            this.wizardPage5.Controls.Add(this.label10);
+            this.wizardPage5.Controls.Add(this.txtCompID);
+            this.wizardPage5.Controls.Add(this.label9);
+            this.wizardPage5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wizardPage5.IsFinishPage = false;
+            this.wizardPage5.Location = new System.Drawing.Point(0, 0);
+            this.wizardPage5.Name = "wizardPage5";
+            this.wizardPage5.Size = new System.Drawing.Size(506, 133);
+            this.wizardPage5.TabIndex = 5;
+            this.wizardPage5.CloseFromNext += new Gui.Wizard.PageEventHandler(this.wizardPage5_CloseFromNext);
+            // 
+            // txtCompName
+            // 
+            this.txtCompName.Location = new System.Drawing.Point(15, 65);
+            this.txtCompName.Name = "txtCompName";
+            this.txtCompName.Size = new System.Drawing.Size(175, 21);
+            this.txtCompName.TabIndex = 3;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 49);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 13);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Name";
+            // 
+            // txtCompID
+            // 
+            this.txtCompID.Location = new System.Drawing.Point(15, 25);
+            this.txtCompID.Name = "txtCompID";
+            this.txtCompID.Size = new System.Drawing.Size(175, 21);
+            this.txtCompID.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "CompetitonID";
+            // 
+            // wizardPage3
+            // 
+            this.wizardPage3.Controls.Add(this.label7);
+            this.wizardPage3.Controls.Add(this.cmbBrikkesysComp);
+            this.wizardPage3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wizardPage3.IsFinishPage = false;
+            this.wizardPage3.Location = new System.Drawing.Point(0, 0);
+            this.wizardPage3.Name = "wizardPage3";
+            this.wizardPage3.Size = new System.Drawing.Size(506, 133);
+            this.wizardPage3.TabIndex = 3;
+            this.wizardPage3.ShowFromNext += new System.EventHandler(this.wizardPage3_ShowFromNext);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 3);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Event";
+            // 
+            // cmbBrikkesysComp
+            // 
+            this.cmbBrikkesysComp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBrikkesysComp.FormattingEnabled = true;
+            this.cmbBrikkesysComp.Location = new System.Drawing.Point(12, 20);
+            this.cmbBrikkesysComp.Name = "cmbBrikkesysComp";
+            this.cmbBrikkesysComp.Size = new System.Drawing.Size(356, 21);
+            this.cmbBrikkesysComp.TabIndex = 0;
+            // 
+            // wizardPage1
+            // 
+            this.wizardPage1.Controls.Add(this.txtPw);
+            this.wizardPage1.Controls.Add(this.label5);
+            this.wizardPage1.Controls.Add(this.txtUser);
+            this.wizardPage1.Controls.Add(this.txtPort);
+            this.wizardPage1.Controls.Add(this.txtHost);
+            this.wizardPage1.Controls.Add(this.label4);
+            this.wizardPage1.Controls.Add(this.label3);
+            this.wizardPage1.Controls.Add(this.label2);
+            this.wizardPage1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wizardPage1.IsFinishPage = false;
+            this.wizardPage1.Location = new System.Drawing.Point(0, 0);
+            this.wizardPage1.Name = "wizardPage1";
+            this.wizardPage1.Size = new System.Drawing.Size(506, 133);
+            this.wizardPage1.TabIndex = 1;
+            // 
+            // txtPw
+            // 
+            this.txtPw.Location = new System.Drawing.Point(72, 89);
+            this.txtPw.Name = "txtPw";
+            this.txtPw.Size = new System.Drawing.Size(171, 21);
+            this.txtPw.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Password";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(72, 62);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(171, 21);
+            this.txtUser.TabIndex = 7;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(72, 34);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(171, 21);
+            this.txtPort.TabIndex = 6;
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(73, 7);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(170, 21);
+            this.txtHost.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Host";
+            // 
+            // NewBrikkesysComp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 181);
+            this.Controls.Add(this.wizard1);
+            this.Name = "NewBrikkesysComp";
+            this.Text = "New Brikkesys-connection";
+            this.wizard1.ResumeLayout(false);
+            this.wizardPage5.ResumeLayout(false);
+            this.wizardPage5.PerformLayout();
+            this.wizardPage3.ResumeLayout(false);
+            this.wizardPage3.PerformLayout();
+            this.wizardPage1.ResumeLayout(false);
+            this.wizardPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Gui.Wizard.Wizard wizard1;
+        private Gui.Wizard.WizardPage wizardPage1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPw;
+        private System.Windows.Forms.Label label5;
+        private Gui.Wizard.WizardPage wizardPage3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbBrikkesysComp;
+        private Gui.Wizard.WizardPage wizardPage5;
+        private System.Windows.Forms.TextBox txtCompID;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtCompName;
+        private System.Windows.Forms.Label label10;
+    }
+}
---
+++ LiveResults.Client/NewBrikkesysComp.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.H2;
+using System.IO;
+using System.Xml.Serialization;
+using LiveResults.Model;
+
+namespace LiveResults.Client
+{
+    public partial class NewBrikkesysComp : Form
+    {
+        public NewBrikkesysComp()
+        {
+            InitializeComponent();
+            txtUser.Text = "root";
+            txtPw.Text = "passwd";
+            txtPort.Text = "3306";
+            RetreiveSettings();
+        }
+
+        [Serializable]
+        public struct Setting
+        {
+            public string Key { get; set; }
+            public string Value { get; set; }
+        }
+        private void RetreiveSettings()
+        {
+            try
+            {
+                if (File.Exists(GetSettingsFile()))
+                {
+                    using (var ms = new MemoryStream(File.ReadAllBytes(GetSettingsFile())))
+                    {
+                        List<Setting> setts = new List<Setting>();
+                        var serializer = new XmlSerializer(setts.GetType());
+                        setts = serializer.Deserialize(ms) as List<Setting>;
+                        if (setts != null)
+                        {
+                        }
+                        applyControlValues(Controls, setts);
+                    }
+                }
+            }
+            catch (Exception ee)
+            {
+
+            }
+        }
+
+        private void applyControlValues(Control.ControlCollection controls, List<Setting> setts)
+        {
+            foreach (Control c in controls)
+            {
+                if (c is TextBox)
+                {
+                    string val = setts.Where(x => x.Key == c.Name).Select(x => x.Value).FirstOrDefault();
+                    (c as TextBox).Text = val;
+                }
+                if (c is ComboBox)
+                {
+                    string val = setts.Where(x => x.Key == c.Name).Select(x => x.Value).FirstOrDefault();
+                    (c as ComboBox).SelectedItem = val;
+                    
+                }
+                if (c is CheckBox)
+                {
+                    string val = setts.Where(x => x.Key == c.Name).Select(x => x.Value).FirstOrDefault();
+                    if (val != null)
+                    {
+                        (c as CheckBox).Checked = val == "True";
+                    }
+                }
+
+                applyControlValues(c.Controls, setts);
+            }
+        }
+
+        private void StoreSettings()
+        {
+            List<Setting> setts = new List<Setting>();
+            extractControlValues(Controls, setts);
+
+            var serializer = new XmlSerializer(setts.GetType());
+            using (var ms = new MemoryStream())
+            {
+                serializer.Serialize(ms, setts);
+                var data = new byte[ms.Length];
+                ms.Seek(0, SeekOrigin.Begin);
+                ms.Read(data, 0, data.Length);
+
+                if (!Directory.Exists(Path.GetDirectoryName(GetSettingsFile())))
+                    Directory.CreateDirectory(Path.GetDirectoryName(GetSettingsFile()));
+
+                File.WriteAllBytes(GetSettingsFile(), data);
+            }
+
+
+        }
+
+        private static string GetSettingsFile()
+        {
+            return Path.Combine(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LiveResults.Client"),
+                "Brikkesysfrm.setts");
+        }
+
+        private void extractControlValues(Control.ControlCollection controls, List<Setting> setts)
+        {
+            foreach (Control c in controls)
+            {
+                if (c is TextBox)
+                {
+                    setts.Add(new Setting
+                    {
+                        Key = (c as TextBox).Name,
+                        Value = (c as TextBox).Text
+                    });
+                }
+                if (c is ComboBox)
+                {
+                    setts.Add(new Setting
+                    {
+                        Key = (c as ComboBox).Name,
+                        Value = (c as ComboBox).SelectedValue as string
+                    });
+                }
+                if (c is CheckBox)
+                {
+                    setts.Add(new Setting
+                    {
+                        Key = (c as CheckBox).Name,
+                        Value = (c as CheckBox).Checked.ToString()
+                    });
+                }
+
+                extractControlValues(c.Controls, setts);
+            }
+        }
+
+
+        private IDbConnection GetDBConnection()
+        {
+            return GetDBConnection("resultatdatabase");
+        }
+
+        private IDbConnection GetDBConnection(string schema)
+        {
+
+            return new MySql.Data.MySqlClient.MySqlConnection("Server=" + txtHost.Text + ";User Id=" + txtUser.Text + ";Port=" + txtPort.Text + ";Password=" + txtPw.Text + (schema != null ? ";Initial Catalog=" + schema : "") + ";charset=utf8;ConnectionTimeout=30");
+        }
+
+        private void wizardPage3_ShowFromNext(object sender, EventArgs e)
+        {
+            StoreSettings();
+            /*Try Connect*/
+            IDbConnection conn = null;
+            try
+            {
+
+                conn = GetDBConnection();
+                conn.Open();
+
+                IDbCommand cmd = conn.CreateCommand();
+                cmbBrikkesysComp.Items.Clear();
+
+                cmd.CommandText = "SELECT id, name, DATE_FORMAT(racedate,'%Y-%m-%d') AS date FROM races ORDER BY date desc";
+
+                IDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    BrikkesysComp cmp = new BrikkesysComp();
+                    cmp.Id = Convert.ToInt32(reader["id"].ToString());
+                    cmp.Name = Convert.ToString(reader["name"]);
+                    cmp.Date = Convert.ToString(reader["date"]);                  
+                    cmbBrikkesysComp.Items.Add(cmp);
+                }
+                reader.Close();
+                cmd.Dispose();
+
+                if (cmbBrikkesysComp.Items.Count > 0)
+                    cmbBrikkesysComp.SelectedIndex = 0;
+
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(this, ee.Message);
+            }
+            finally
+            {
+                if (conn != null)
+                    conn.Close();
+            }
+        }
+
+        private class BrikkesysComp
+        {
+            public string Name;
+            public int Id;
+            public string Date;
+
+            public override string ToString()
+            {
+                return Date + " | " + Name;
+            }
+        }
+
+        
+        private void wizardPage5_CloseFromNext(object sender, Gui.Wizard.PageEventArgs e)
+        {
+            StoreSettings();
+            //start
+            FrmMonitor monForm = new FrmMonitor();
+            this.Hide();
+            //BrikkesysParser pars = new BrikkesysParser(GetDBConnection((cmbBrikkesysComp.SelectedItem as BrikkesysComp).Id), chkCreateRadioControls.Checked);
+            //monForm.SetParser(pars as IExternalSystemResultParser);
+            monForm.CompetitionID = Convert.ToInt32(txtCompID.Text);
+            monForm.ShowDialog(this);
+        }
+
+       
+    }
+}
