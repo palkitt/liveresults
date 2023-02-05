65ba13939f06bc17c0af5c9e662f3875e0f20d5a
palkitt <44671818+palkitt@users.noreply.github.com>
2023-02-05 14:58:06 +0100

    Add version date

--- LiveResults.Client/FrmETimingMonitor.designer.cs
+++ LiveResults.Client/FrmETimingMonitor.designer.cs
@@ -96,7 +96,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmETimingMonitor";
-            this.Text = "eTiming Monitor. Version 2023-01-14";
+            this.Text = "eTiming Monitor. Ver 2023-02-05";
             this.Load += new System.EventHandler(this.FrmETimingMonitor_Load);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
