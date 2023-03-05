cbe01f1a8420006be2592086503cb07467acf1af
palkitt <44671818+palkitt@users.noreply.github.com>
2023-03-05 17:55:19 +0100

    Update links with new start registration

--- web/adm/radiolinks.php
+++ web/adm/radiolinks.php
@@ -39,55 +39,44 @@ function startRadioControl() {
 </script>
 
 </head>
-
 <body topmargin="0" leftmargin="0">
-
-
-<!-- MAIN DIV -->
 
 <div class="maindiv">
 <table border="0" cellpadding="0" cellspacing="0" >
   <tr>
-     <td valign="bottom">
-<!-- MAIN MENU FLAPS - Two rows, note that left and right styles differs from middle ones -->
-
-     <table border="0" cellpadding="0" cellspacing="0">
-          <!-- Top row with rounded corners -->
-          <tr>
-               <td colspan="4"><span class="mttop"></td></tr>
-     </table>
-     </td><td align="right" valign="bottom"></td></tr>
-  <tr><td class="submenu" colspan="2">
+    <td valign="bottom">
+      <table border="0" cellpadding="0" cellspacing="0">
+        <tr><td colspan="4"><span class="mttop"></td></tr>
+      </table>
+    </td>
+    <td align="right" valign="bottom"></td>
+  </tr>
+  <tr>
+    <td class="submenu" colspan="2">
        <table border="0" cellpadding="0" cellspacing="0">
-             <tr>
-               <td><?=$currentComp->CompName()?>, <?=$currentComp->Organizer()?> [<?=$currentComp->CompDate()?>]</td>
-             </tr>
+        <tr>
+          <td><?=$currentComp->CompName()?>, <?=$currentComp->Organizer()?> [<?=$currentComp->CompDate()?>]</td>
+        </tr>
        </table>
      </td>
   </tr>
-<!-- End SUB MENU -->
+  <tr>
+    <td class="searchmenu" colspan="2" style="padding: 5px;">
+      <h1 class="categoriesheader">Radiolinks</h1>
 
-<tr><td class="searchmenu" colspan="2" style="padding: 5px;">
-<h1 class="categoriesheader">Radiolinks</h1>
-
-<table border="0" cellpadding="0" cellspacing="2" width="100%">
-<tr><td><b>Type</b></td><td><b>Kode</b></td></tr>
-<tr><td><a href="..\radio.php?comp=<?=$_GET['comp']?>&code=0">Start</a></td><td>0</td></tr>
-<tr><td><a href="..\radio.php?comp=<?=$_GET['comp']?>&code=1000">Mål</a></td><td>1000</td></tr>
-<tr><td><a href="..\radio.php?comp=<?=$_GET['comp']?>&code=-2">I løypa</a></td><td>-2</td></tr>
-<tr><td><a href="..\radio.php?comp=<?=$_GET['comp']?>&code=-1">Alle meldeposter</a></td><td>-1</td></tr>
-<tr><td><button onclick="startRadioControl()">En meldepost</button></td><td>[kode]</td></tr>
-</table>
-</td></tr>
-
-</table>
-     </td>
+      <table border="0" cellpadding="0" cellspacing="2" width="100%">
+      <tr><td><b>Type</b></td><td><b>Kode</b></td></tr>
+      <tr><td><a href="..\radio.php?comp=<?=$_GET['comp']?>&code=0">Start</a></td><td>0</td></tr>
+      <tr><td><a href="..\radio.php?comp=<?=$_GET['comp']?>&code=-10">Start (new version)</a></td><td>-10</td></tr>
+      <tr><td><a href="..\radio.php?comp=<?=$_GET['comp']?>&code=1000">Mål</a></td><td>1000</td></tr>
+      <tr><td><a href="..\radio.php?comp=<?=$_GET['comp']?>&code=-2">Igjen i løypa</a></td><td>-2</td></tr>
+      <tr><td><a href="..\radio.php?comp=<?=$_GET['comp']?>&code=-1">Alle meldeposter</a></td><td>-1</td></tr>
+      <tr><td><button onclick="startRadioControl()">En meldepost</button></td><td>[kode]</td></tr>
+      </table>
+    </td>
   </tr>
 </table>
-<br/>
-
 
 </div>
-<br/><br/>
 </body>
 </html>
