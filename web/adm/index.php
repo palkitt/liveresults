777dcca3a1a77467e0e35383eaaa689697908446
palkitt <pal.kittilsen@gmail.com>
2023-10-22 07:14:11 +0200

    Add shorcuts for radio control editor and ... remaining runners Cleanup of page code

--- web/adm/index.php
+++ web/adm/index.php
@@ -1,24 +1,13 @@
 <?php include_once("../templates/emmalang_sv.php");
-
 	include_once("../templates/classEmma.class.php");
-
-   $lang = "en";
-
-   if (isset($_GET['lang']) && $_GET['lang'] != "")
-
-   {
-
-	$lang = $_GET['lang'];
-
-   }
-
+  $lang = "en";
+  if (isset($_GET['lang']) && $_GET['lang'] != "")
+    $lang = $_GET['lang'];
 include_once("../templates/emmalang_$lang.php");
-
 header('Content-Type: text/html; charset='.$CHARSET);
-
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-        "http://www.w3.org/TR/html4/loose.dtd">
+  "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 
 <head><title><?=$_TITLE?></title>
@@ -27,7 +16,6 @@ header('Content-Type: text/html; charset='.$CHARSET);
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="mobile-web-app-capable" content="yes">
 <meta name="robots" content="noindex">
-
 
 <link rel="stylesheet" type="text/css" href="../css/style-freidig.css">
 <link rel="stylesheet" type="text/css" href="../css/style-admin.css">
@@ -59,81 +47,81 @@ function resetRow(row)
 <div class="maindiv">
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
   <tr>
-     <td valign="bottom">
-<!-- MAIN MENU FLAPS - Two rows, note that left and right styles differs from middle ones -->
-
-     <table border="0" cellpadding="0" cellspacing="0">
-          <!-- Top row with rounded corners -->
-          <tr>
-               <td colspan="4"><span class="mttop"></td>
-          </tr>
-     </table>
-     </td>
-     <td align="right" valign="bottom">
-     </td>
+    <td valign="bottom">
+      <table border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td colspan="4"><span class="mttop"></td>
+        </tr>
+      </table>
+    </td>
+    <td align="right" valign="bottom"></td>
   </tr>
   <tr>
     <td class="submenu" colspan="2">
-       <table border="0" cellpadding="0" cellspacing="0" width="100%">
-             <tr>
-               <td><a href="createComp.php">Create new competition</a> | <a href="../">Show competitions</a> </td>
-             </tr>
-       </table>
-     </td>
+      <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+          <td>
+            <a href="createComp.php">Create new competition</a> | <a href="../">Show competitions</a>
+          </td>
+        </tr>
+      </table>
+    </td>
   </tr>
-<!-- End SUB MENU -->
-
-<tr> <td class="searchmenu" colspan="2" style="padding: 5px;">
-<table border="0" cellpadding="0" cellspacing="0">
-<tr><td>
-<h1 class="categoriesheader">Existing competitions</h1>
-
-<table border="0" cellpadding="0" cellspacing="2" width="100%">
-<tr>
-<td><b>Date</b></td>
-<td><b>&nbsp;&nbsp;Competition</b></td>
-<td><b>Organizer</b></td>
-<td><b>CompId</b></td>
-<td><b>Public</b></td>
-<td><b>LiveCenter</b></td>
-</tr>
-
+  <tr> 
+    <td class="searchmenu" colspan="2" style="padding: 5px;">
+      <table border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td>
+            <h1 class="categoriesheader">Existing competitions</h1>
+            <table border="0" cellpadding="0" cellspacing="2" width="100%">
+              <tr>
+                <td><b>Date</b></td>
+                <td><b>&nbsp;&nbsp;Competition</b></td>
+                <td><b>Organizer</b></td>
+                <td><b>CompId</b></td>
+                <td><b>Public</b></td>
+                <td><b>LiveCenter</b></td>
+              </tr>
 <?php
-
 	$comps = Emma::GetAllCompetitions();
 	
 	foreach ($comps as $comp)
 	{
 	?>
 		<tr id="row<?=$comp["tavid"]?>">
-    <td><?=date("Y-m-d",strtotime($comp['compDate']))?></td>
-    <td>
-    <div class="dropdownAdm">
-    <button class="dropbtnAdm"><?=$comp["compName"]?></button>
-    <div class="dropdownAdm-content">
-    <a href="/adm/editComp.php?compid=<?=$comp["tavid"]?>">Edit competition</a>
-    <a href="/followfull.php?comp=<?=$comp['tavid']?>">Results page</a>
-    <a href="/message.php?comp=<?=$comp["tavid"]?>">Messages</a>
-    <a href="/radio.php?comp=<?=$comp["tavid"]?>&code=0">Start registration</a>
-    <a href="/speaker.php?comp=<?=$comp["tavid"]?>">Speaker view</a>
-    <a href="/adm/radiolinks.php?comp=<?=$comp["tavid"]?>">Radio links</a>
-    <a href="/adm/runners.php?compid=<?=$comp["tavid"]?>">Runner list and edit</a>
-    <a href="/startlist.php?comp=<?=$comp["tavid"]?>">Startlist</a>
-  </div>
-  </div></td>
-    <td><?=$comp["organizer"]?></td>
-    <td><?=$comp["tavid"]?></td>
-    <td><?=$comp["public"] == "1" ? "yes" : "no"?></td>
-    <?php if (strlen($comp['livecenterurl'])>0){ ?>  <td><a href="../<?=$comp['livecenterurl']?>">LC</a></td>	<?php } ?>
+      <td><?=date("Y-m-d",strtotime($comp['compDate']))?></td>
+      <td>
+        <div class="dropdownAdm">
+          <button class="dropbtnAdm"><?=$comp["compName"]?></button>
+          <div class="dropdownAdm-content">
+            <a href="/adm/editComp.php?compid=<?=$comp["tavid"]?>">Edit competition</a>
+            <a href="/followfull.php?comp=<?=$comp['tavid']?>">Results page</a>
+            <a href="/speaker.php?comp=<?=$comp["tavid"]?>">Speaker view</a>
+            <a href="/message.php?comp=<?=$comp["tavid"]?>">Messages</a>
+            <a href="/radio.php?comp=<?=$comp["tavid"]?>&code=0">Start registration</a>
+            <a href="/adm/radiolinks.php?comp=<?=$comp["tavid"]?>">Radio links</a>
+            <a href="/adm/runners.php?compid=<?=$comp["tavid"]?>">Runner list and edit</a>
+            <a href="/startlist.php?comp=<?=$comp["tavid"]?>">Startlist </a>
+            <a href="/adm/radiocontrols.php?comp=<?=$comp["tavid"]?>">Radio control editor</a>
+            <a href="/radio.php?comp=<?=$comp["tavid"]?>&code=-2">Remaining runners</a>
+          </div>
+        </div>
+      </td>
+      <td><?=$comp["organizer"]?></td>
+      <td><?=$comp["tavid"]?></td>
+      <td><?=$comp["public"] == "1" ? "yes" : "no"?></td>
+      <?php if (strlen($comp['livecenterurl'])>0){ ?>  
+        <td><a href="../<?=$comp['livecenterurl']?>">LC</a></td>
+      <?php } ?>
     </tr>
 	<?php
 	}
 	?>
-			</table>
-		</td>
-	     </tr>
-	</table>
-     </td>
+			      </table>
+		      </td>
+	      </tr>
+	    </table>
+    </td>
   </tr>
 </table>
 </div>
