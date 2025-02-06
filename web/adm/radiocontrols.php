93b1f9b9ae41ff8aa78152b5577dac3473bf71ff
palkitt <pal.kittilsen@gmail.com>
2025-02-06 18:50:19 +0100

    fix: update to remote datatables source

--- web/adm/radiocontrols.php
+++ web/adm/radiocontrols.php
@@ -2,6 +2,7 @@
 
 include_once("../templates/emmalang_en.php");
 include_once("../templates/classEmma.class.php");
+include_once("../templates/datatablesURL.php");
 header('Content-Type: text/html; charset=' . $CHARSET);
 
 $currentComp = new Emma($_GET['comp']);
@@ -19,17 +20,11 @@ echo ("<?xml version=\"1.0\" encoding=\"$CHARSET\" ?>\n");
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="theme-color" content="#555556">
-  <link rel="stylesheet" href="https://cdn.datatables.net/v/dt/jq-3.7.0/dt-2.1.8/fc-5.0.3/fh-4.0.1/datatables.min.css">
-  <link rel="stylesheet" href="../css/style-liveres.css?a">
 
-  <script src="https://cdn.datatables.net/v/dt/jq-3.7.0/dt-2.1.8/fc-5.0.3/fh-4.0.1/datatables.min.js"></script>
-  <script src="js/messages.js"></script>
-
-
-
-  <script language="javascript" type="text/javascript" src="../js/jquery-3.7.0.min.js"></script>
-  <script language="javascript" type="text/javascript" src="../js/jquery.dataTables.min.js"></script>
-  <script language="javascript" type="text/javascript" src="../js/dataTables.fixedHeader.min.js"></script>
+  <link rel="stylesheet" href="<?= $DataTablesURL ?>datatables.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/style-liveres.css">
+  <script src="<?= $DataTablesURL ?>datatables.min.js"></script>
+  <script src="../js/messages.js"></script>
   <script language="javascript" type="text/javascript">
     var codes = null;
     var classes = null;
