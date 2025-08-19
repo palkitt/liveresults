25b652a936abf0daa2de88f1d68c4b70a66decc1
palkitt <pal.kittilsen@gmail.com>
2025-08-19 21:11:37 +0200

    include  DataTables files to site. cleanup

--- web/followfull.php
+++ web/followfull.php
@@ -85,9 +85,7 @@ echo ("<?xml version=\"1.0\" encoding=\"$CHARSET\" ?>\n");
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="theme-color" content="#555555">
-  <!-- link rel="stylesheet" href="css/datatables.min.css">
-  <link rel="stylesheet" href="css/font-awesome/6.6.0/css/all.min.css">
-  <script src="js/datatables.min.js"></script -->
+  <!-- link rel="stylesheet" href="css/font-awesome/6.6.0/css/all.min.css" -->
   <link rel="stylesheet" href="<?= $DataTablesURL ?>datatables.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
   <link rel="stylesheet" href="css/style-liveres.css?a">
